c0187d9ba4cab52fab4764f1e9777284f4b537fc
Jadeilson Martinho <jadeilsonm17@gmail.com>
2024-05-08 21:25:40 -0300

    feat: implementation oh the methods

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,21 +1,80 @@
 import java.util.LinkedList;
 
-public class HashTable {
-
-    private LinkedList<KeyValue> table;
-    private Integer size;
+public class HashTable<Key, Value> {
     private Integer sizeMix = 10;
+    private LinkedList<KeyValue<Key, Value>>[] table;
+    private Integer size;
 
-    public void put(String key, Object value) {}
+    public HashTable(int sizeMix) {
+        this.sizeMix = sizeMix;
+        size = 0;
+        table = (LinkedList<KeyValue<Key, Value>>[]) new LinkedList[sizeMix];
+        for (int i = 0; i < sizeMix; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
 
-    public void get(String key) {}
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % sizeMix;
+    }
 
-    public void remove(String key) {}
+    public void put(Key key, Value val) {
+        int i = hash(key);
+        for (KeyValue<Key, Value> kv : table[i]) {
+            if (kv.key.equals(key)) {
+                kv.value = val;
+                return;
+            }
+        }
+        table[i].add(new KeyValue<>(key, val));
+        size++;
+    }
+
+    public Value get(Key key) {
+        int i = hash(key);
+        for (KeyValue<Key, Value> kv : table[i]) {
+            if (kv.key.equals(key)) {
+                return kv.value;
+            }
+        }
+        return null;
+    }
+
+    public void delete(Key key) {
+        int i = hash(key);
+        for (KeyValue<Key, Value> kv : table[i]) {
+            if (kv.key.equals(key)) {
+                table[i].remove(kv);
+                size--;
+                return;
+            }
+        }
+    }
+
+    public void update(Key key, Value val) {
+        int i = hash(key);
+        for (KeyValue<Key, Value> kv : table[i]) {
+            if (kv.key.equals(key)) {
+                kv.value = val;
+                return;
+            }
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private static class KeyValue<Key, Value> {
+        Key key;
+        Value value;
+
+        public KeyValue(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
 
-class KeyValue<T> {
-    private String key;
-    private LinkedList<T> value;
-}
 
 
